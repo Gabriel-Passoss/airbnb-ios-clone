a13ee3d4aa020c4c75ba855959077b4e8f10f368
Gabriel Passos <contato.gabrielpassoss@gmail.com>
2024-08-21 11:09:21 -0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Gabriel dos Passos on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
